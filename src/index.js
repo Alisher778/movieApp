0eb295936f3be91a25f126835891798005acdad4
Alisher778 <internet-docs@mail.ru>
2018-08-03 23:50:57 +0500

    added reactstrap and navbar

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import Navbar from "./components/partials/Navbar.jsx";
+import registerServiceWorker from "./registerServiceWorker";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Navbar />, document.getElementById("root"));
 registerServiceWorker();
